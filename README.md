05872662e44eaab379ab95523fc6e6ec29eaa842
sysr-q <sysreq@riseup.net>
2014-03-11 22:06:06 +1100

    Restructured the packages, split gopp from gppc.

--- README.md
+++ README.md
@@ -3,5 +3,50 @@ gopp - Go Pre-Processor
 
 Why? I honestly could not tell you.
 
-http://stackoverflow.com/a/13889804 has better solutions - Go build flags and
-tags. Just use them.
+Go has something like this built in - compiler build flags and tags. They're
+definitely a much more sane solution, so if at all possible, you should be
+using those instead of something like gopp.
+
+[Stack Overflow](http://stackoverflow.com/a/13889804) has more information, if
+you're interested in that topic.
+
+A lot of the key ideas and inspiration was taken from _bytbox_'s [goprep](https://github.com/bytbox/goprep)
+project, which hasn't been updated in two years. Both projects are MIT Licensed.
+
+-----
+
+If, for some strange reason, you're still interested in gopp, there are a few
+features (mainly modeled after the _c preprocessor_), which you can use:
+
+_NB!_ As a general rule, gopp rules are _comments, prefixed with `//gopp:`!_
+e.g., `//gopp:ifdef DEBUG`
+
+| command   | description
+|:----------|:-----------
+| `ifdef`   | _ifdef_ allows you to test whether or not a certain macro is defined at the time of interpretation.
+| `ifndef`  | _ifndef_ is the opposite of ifdef, only stepping in if a certain macro is _not_ defined.
+| `else`    | _else_ works like an else block in a C-like programming language.
+| `endif`   | _endif_ works like a closing bracket on an else block in a C-like programming language.
+| `define`  | _define_ allows you to define arbitrary macros on the fly; e.g. `//gopp:define DEBUG true`
+| `undef`   | _undef_ allows you to revoke currently-defined macros.
+
+`if` and `elseif` are not supported: they'd make things needlessly complex, and
+are more work than I'd like to put in - if you want something like this, you
+should probably use Go's compiler flags/tags instead.
+
+gopp
+----
+
+This is the main library for using the preprocessor. The import path is `github.com/sysr-q/gopp/gopp`.
+
+You can view the [godocs](http://godoc.org/github.com/sysr-q/gopp/gopp) if
+you're interested in using it.
+
+gppc
+----
+
+_gppc_ is a command line client that allows you to easily process Go code with
+gopp, in a similar vein to the unix tool, `cpp`.
+
+To install: `go install github.com/sysr-q/gopp/gppc`, then check `gppc` and
+`gppc build --help`.
--- gopp/gopp.go
+++ gopp/gopp.go
@@ -1,9 +1,8 @@
-package main
+package gopp
 
 import (
 	"bytes"
 	"fmt"
-	"github.com/droundy/goopt"
 	"go/parser"
 	"go/printer"
 	"go/scanner"
@@ -90,8 +89,6 @@ func (g *gopp) Parse(r io.Reader) error {
 
 			cmd := strings.ToLower(lnr[0])
 
-			//fmt.Printf("%q %q %s %v\n", strTrim, cmd, lnr, g.ignoring)
-
 			if cmd == "ifdef" {
 				if len(lnr) != 2 {
 					continue
@@ -151,144 +148,13 @@ func (g *gopp) Reset() {
 	g.ignoring = false
 }
 
-func NewGopp(strip bool) *gopp {
+// New creates a new gopp instance with sane defaults.
+func New(strip bool) *gopp {
 	return &gopp{
 		defines:       make(map[string]interface{}),
 		output:        make(chan Token),
 		StripComments: strip,
 		ignoring:      false,
 		Prefix:        "//gopp:",
-	}
-}
-
-///////////////////
-
-func IsDirectory(path string) (bool, error) {
-	dir, err := os.Stat(path)
-	if err == nil {
-		return dir != nil && dir.IsDir(), nil
-	}
-	if os.IsNotExist(err) {
-		return false, err
-	}
-	return false, err
-}
-
-func build() error {
-	defined := goopt.Strings(
-		[]string{"-D"},
-		"NAME[=defn]",
-		"Predefine NAME as a macro. Unless given, default macro value is 1.")
-	undefined := goopt.Strings([]string{"-U"},
-		"NAME",
-		"Cancel any previous/builtin definition of macro NAME.")
-	stripComments := goopt.Flag([]string{"-c", "--comments"},
-		[]string{"-C", "--no-comments"},
-		"Don't eat comments.",
-		"Eat any comments that are found.")
-	outputTo := goopt.String([]string{"-o"},
-		"-",
-		"Output (default: <stdout> for files, `_build` for directories)")
-	/*panicErrors := goopt.Flag([]string{"-p", "--panic"},
-	[]string{},
-	"panic() on errors, rather than just logging them.",
-	"")*/
-
-	// Because you can't supply an arg list to goopt..
-	if len(os.Args) > 2 {
-		os.Args = append([]string{os.Args[0]}, os.Args[2:]...)
-	} else {
-		os.Args = os.Args[:1]
-	}
-
-	goopt.Description = func() string {
-		return "What have I done?!"
-	}
-	goopt.Version = "0.2"
-	goopt.Summary = "Horrifying C-like Go preprocessor."
-	goopt.Parse(nil)
-
-	if len(goopt.Args) < 1 {
-		fmt.Println("Supply a file to process! Here's --help:")
-		fmt.Print(goopt.Help())
-		return nil
-	}
-
-	g := NewGopp(true)
-	g.StripComments = !*stripComments
-	g.DefineValue("_GOPP", goopt.Version)
-	for _, def := range *defined {
-		if strings.Contains(def, "=") {
-			lnr := strings.SplitN(def, "=", 2)
-			g.DefineValue(lnr[0], lnr[1])
-		} else {
-			g.DefineValue(def, 1)
-		}
-	}
-
-	for _, udef := range *undefined {
-		g.Undefine(udef)
-	}
-
-	isDir, err := IsDirectory(goopt.Args[0])
-	if err != nil {
-		return err
-	}
-
-	if isDir {
-		// TODO: Handle walking path and parsing.
-	} else {
-		file, err := os.Open(goopt.Args[0])
-		if err != nil {
-			return err
-		}
-
-		if err := g.Parse(file); err != nil {
-			return err
-		}
-
-		var out io.Writer
-		if *outputTo == "-" {
-			out = os.Stdout
-		} else {
-			out, err = os.Open(*outputTo)
-			if err != nil {
-				return err
-			}
-		}
-		g.Print(out)
-	}
-	return nil
-}
-
-func clean() error {
-	return nil
-}
-
-var commands = map[string]func() error{
-	"build": build,
-	"clean": clean,
-}
-
-func main() {
-	printWrong := func() {
-		fmt.Println("Wrong! Try one of these: (you can append --help)")
-		for c := range commands {
-			fmt.Printf("  %s %s\n", os.Args[0], c)
-		}
-	}
-
-	if len(os.Args) < 2 {
-		printWrong()
-		return
-	}
-
-	f, ok := commands[os.Args[1]]
-	if !ok {
-		printWrong()
-		return
-	}
-	if err := f(); err != nil {
-		panic(err)
 	}
 }
